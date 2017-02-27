884e3f604566857d57dd57b0f8fd9a0d0112dac3
maverick-12 <wh4ckin9@hotmail.it>
2017-02-27 10:25:34 +0100

    Beginning template

---
+++ WOL/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WOL
+//
+//  Created by Roberto Fregiaro on 27/02/17.
+//  Copyright © 2017 Marco Fregiaro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
