73084291c60dfcbfc3dfd4eee3b7d54ad0f243aa
maverick-12 <wh4ckin9@hotmail.it>
2017-02-28 21:00:10 +0100

    Raw insertion SwiftSocket

--- WOL/FirstViewController.swift
+++ WOL/FirstViewController.swift
@@ -9,6 +9,60 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    let host = "192.168.91.100"
+    let port = 7
+    var client: UDPClient?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        client = UDPClient(address: host, port: Int32(port))
+    }
+    
+    @IBAction func sendButtonAction() {
+        guard let client = client else { return }
+        
+        switch client.connect(timeout: 10) {
+        case .success:
+            appendToTextField(string: "Connected to host \(client.address)")
+            if let response = sendRequest(string: "GET / HTTP/1.0\n\n", using: client) {
+                appendToTextField(string: "Response: \(response)")
+            }
+        case .failure(let error):
+            appendToTextField(string: String(describing: error))
+        }
+    }
+    
+    private func sendRequest(string: String, using client: UDPClient) -> String? {
+        appendToTextField(string: "Sending data ... ")
+        
+        switch client.send(string: string) {
+        case .success:
+            return readResponse(from: client)
+        case .failure(let error):
+            appendToTextField(string: String(describing: error))
+            return nil
+        }
+    }
+    
+    private func readResponse(from client: UDPClient) -> String? {
+        guard let response = client.read(1024*10) else { return nil }
+        
+        return String(bytes: response, encoding: .utf8)
+    }
+    
+    private func appendToTextField(string: String) {
+        print(string)
+        textView.text = textView.text.appending("\n\(string)")
+    }
+
+    
+    
+    // ------------------------------------------------------------------
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- WOL/SecondViewController.swift
+++ WOL/SecondViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    
+    // ------------------------------------------------------------------------
 
     override func viewDidLoad() {
         super.viewDidLoad()
